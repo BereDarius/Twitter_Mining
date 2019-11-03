608e78d56d4b9d815790c72f248297f6f659c3c9
Bere Darius <tuborg.bere3080@gmail.com>
2019-11-03 10:35:10 +0200

    Added readme.txt file

---
+++ readme.txt
@@ -0,0 +1,3 @@
+Twitter Mining
+
+I am following a bunch of Python tutorials and this is part of them.
---
+++ twitter_data_mining.py
@@ -0,0 +1,55 @@
+import tweepy
+import json
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+consumer_key = 'Gouy4NlHSLTiOwtYzZSjswaNm'
+consumer_secret = 'llZDt0AtaO6kazv82c70zs9f0rrpXK3d2Qcr5V1MvaYUIpfoEw'
+access_token = '1193240172-hqE2lacVTo6EenxRJx0qstHEQd7bThSZr69jZfR'
+access_secret = 'CJm3SIbeNz8vaFdSYYHmY3tA4ldeFE9ovIjD0TavgMv10'
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+
+def process_or_store(tweet):
+    print(json.dumps(tweet))
+
+
+# Read our own timeline (10 items)
+
+for status in tweepy.Cursor(api.home_timeline).items(10):
+    process_or_store(status._json)
+
+# List of all our followers
+
+for friend in tweepy.Cursor(api.friends).items():
+    process_or_store(friend._json)
+
+# List of all our tweets
+
+for tweet in tweepy.Cursor(api.user_timeline).items():
+    process_or_store(tweet._json)
+
+
+class MyListener(StreamListener):
+
+    def on_data(self,data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#python'])
